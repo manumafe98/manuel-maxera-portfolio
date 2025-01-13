14d9daa054ea077bb9b75d74fb0ee1970b012b9d
manumafe98 <manumaxerapro@gmail.com>
2025-01-12 22:58:39 -0300

    Improve logic to avoid code repetition using map and a array of objects

--- Portfolio/src/components/ApplicationsMenuComponent.jsx
+++ Portfolio/src/components/ApplicationsMenuComponent.jsx
@@ -9,45 +9,57 @@ export const ApplicationsMenuComponent = ({ openIconDialog }) => {
     openIconDialog(event.currentTarget.dataset.value)
   }
 
+  const applications = [
+    {
+      name: "Terminal",
+      dataValue: "terminal",
+      imgSrc: terminal_app,
+      imgAlt: "Ubuntu Terminal App Icon"
+    },
+    {
+      name: "About Me",
+      dataValue: "sublime",
+      icon: SublimeTextIcon
+    },
+    {
+      name: "Working Experience",
+      dataValue: "vscode",
+      icon: VisualStudioCodeIcon
+    },
+    {
+      name: "Contact Me",
+      dataValue: "gmail",
+      icon: GmailIcon
+    },
+    {
+      name: "My Projects",
+      dataValue: "ubuntusoftware",
+      imgSrc: software_store,
+      imgAlt: "Ubuntu Store App Icon"
+    },
+    {
+      name: "My Skills",
+      dataValue: "filemanager",
+      imgSrc: file_manager,
+      imgAlt: "Ubuntu File Manager App Icon"
+    }
+  ]
+
   return (
     <div className="bg-ubuntu-dock/90 h-screen w-24 opacity-65 absolute z-1 border-1 border-solid border-[#464646] flex flex-col py-4">
       <ul className="flex flex-col items-center grow-[0.90]">
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="terminal">
-          <img src={terminal_app} alt="Ubuntu Terminal App Icon" className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            Terminal
-          </div>
-        </li>
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="sublime">
-          <SublimeTextIcon className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            About Me
-          </div>
-        </li>
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="vscode">
-          <VisualStudioCodeIcon className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            Working Experience
-          </div>
-        </li>
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="gmail">
-          <GmailIcon className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            Contact Me
-          </div>
-        </li>
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="ubuntusoftware">
-          <img src={software_store} alt="Ubuntu Store App Icon" className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            My Projects
-          </div>
-        </li>
-        <li className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value="filemanager">
-          <img src={file_manager} alt="Ubuntu File Manager App Icon" className="w-14 h-14"/>
-          <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
-            My Skills
-          </div>
-        </li>
+        {applications.map((application, index) => (
+          <li key={index} className="mb-2 rounded-xl p-3 hover:bg-gray-50/10 relative group cursor-pointer" onClick={handleAppIconClick} data-value={application.dataValue}>
+            {application.icon ? (
+              <application.icon className="w-14 h-14"/>
+            ) : (
+              <img src={application.imgSrc} alt={application.imgAlt} className="w-14 h-14"/>
+            )}
+            <div className="absolute left-full ml-2 top-1/2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap select-none">
+              {application.name}
+            </div>
+          </li>
+        ))}
       </ul>
       <div className="flex justify-center">
         <MenuIcon className="fill-current text-white w-14 h-14"/>
